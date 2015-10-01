6d8c1c94ac70b67b055f13e55427ce39fe96f29e
shalomvolchok <shalomvolchok@gmail.com>
2015-10-01 14:16:36 +0300

    just working

--- src/server.js
+++ src/server.js
@@ -8,12 +8,14 @@ var express = require('express');
 var favicon = require('serve-favicon');
 var serialize = require('serialize-javascript');
 var navigateAction = require('./app/actions/navigate');
-var debug = require('debug')('Example');
 var React = require('react');
 var app = require('./app');
 var HtmlComponent = React.createFactory(require('./Html.js'));
 var Router = require('react-router');
 var FluxibleComponent = require('fluxible-addons-react/FluxibleComponent');
+
+// Allows hashed names for .js and .css files
+var webpackStats = require("./webpack/utils/webpack-stats.json");
 
 // Needed to parse posts
 var bodyParser = require('body-parser');
@@ -25,15 +27,20 @@ var csrf = require('csurf');
 var server = express();
 server.set('state namespace', 'App');
 
+/* __DEV__ */
+var path = require('path');
+var webpack = require('webpack');
+var config = require('./webpack/dev.config');
+var compiler = webpack(config);
+server.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+server.use(require('webpack-hot-middleware')(compiler));
+/* __END_DEV__ */
+
 server.use(favicon(__dirname + '/favicon.ico'));
-
-/* __PROD__ */
 server.use('/public', express.static(__dirname + '/public'));
-/* __END_PROD__ */
-
-// On development, serve the static files from the webpack dev server.
-require("./webpack/hotServer");
-
 server.use(cookieParser());
 server.use(bodyParser.json());
 server.use(csrf({cookie: true}));
@@ -41,12 +48,6 @@ server.use(csrf({cookie: true}));
 var fetchrPlugin = app.getPlugin('FetchrPlugin');
 fetchrPlugin.registerService(require('./app/services/exampleService'));
 server.use(fetchrPlugin.getXhrPath(), fetchrPlugin.getMiddleware());
-
-var require("./webpack/utils/webpack-stats.json");
-
-/* __DEV__ */
-delete require.cache[require.resolve("./webpack/utils/webpack-stats.json")];
-/* __END_DEV__ */
 
 server.use(function (req, res, next) {
 
@@ -82,6 +83,6 @@ server.use(function (req, res, next) {
     });
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3005;
 server.listen(port);
 console.log('Listening on port ' + port);
